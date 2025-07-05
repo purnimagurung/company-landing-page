9a0cc578abd382db0548e8e59a196a78e7c45dd9
purnimagurung <grg.puru@gmail.com>
2025-07-05 10:27:40 +0200

    company-landing-page-react-tailwind

---
+++ src/components/header/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import Logos from "../../assets/images/Logo.png";
+
+const Logo = () => (
+  <Link to="/" className="flex items-center space-x-2">
+    {/* <img src={Logos} alt="Logo" className="h-8" /> */}
+    <span className='text-lg'>CompanyLogo</span>
+  </Link>
+);
+
+export default Logo;
